68a4bc07360341645290759aed33acbd680dcfdc
SanjaySodani <88200484+SanjaySodani@users.noreply.github.com>
2021-12-30 17:19:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    const text = document.querySelector("#text");
+
+    setTimeout(()=>{
+        text.innerText = 9;
+        setTimeout(()=>{
+            text.innerText = 8;
+            setTimeout(()=>{
+                text.innerText = 7;
+                setTimeout(()=>{
+                    text.innerText = 6;
+                    setTimeout(()=>{
+                        text.innerText = 5;
+                        setTimeout(()=>{
+                            text.innerText = 4;
+                            setTimeout(()=>{
+                                text.innerText = 3;
+                                setTimeout(()=>{
+                                    text.innerText = 2;
+                                    setTimeout(()=>{
+                                        text.innerText = 1;
+                                        setTimeout(()=>{
+                                            text.innerText = "Happy Independence Day!";
+                                        }, 500);
+                                    }, 500);
+                                }, 500);
+                            }, 500);
+                        }, 500);
+                    }, 500);
+                }, 500);
+            }, 500);
+        }, 500);
+    },500);
+}
